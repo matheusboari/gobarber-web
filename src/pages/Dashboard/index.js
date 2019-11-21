d02a5c38a5893e3faade69384ae5146d023bffc9
Matheus Boari <matheusboari@gmail.com>
2019-11-21 05:21:53 -0300

    Style dashboard :lipstick:

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,41 @@
 import React from 'react';
 import api from '~/services/api';
 
-// import { Container } from './styles';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#FFF" />
+        </button>
+        <strong>21 de Novembro</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#FFF" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Matheus Boari</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Matheus Boari</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Matheus Boari</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
